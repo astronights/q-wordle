42120b248fda5f9c877cf240eda0a515dd60c4fe
astronights <shubhankar.a31@gmail.com>
2022-10-16 11:19:40 +0800

    Basic Q function (To be tested)

--- src/models/qlearn.py
+++ src/models/qlearn.py
@@ -1,30 +1,44 @@
 from .base_model import BaseModel
 from ..strategies.random import RandomStrategy
+from ..envs.qwordle import QWordle
+from ..utils import get_state, word_to_action
 
 import numpy as np
 
 class QLearn(BaseModel):
     
-    def __init__(self, config):
+    def __init__(self, config = None):
         super().__init__(config)
         self.strategies = []
         self.strategies.append(RandomStrategy())
+        self.Q = np.zeros((6, 6, 26, len(self.strategies)))
+        self.epsilon = 0.1
+        self.gamma = 0.6
+        self.env = QWordle()
 
-    def createEpsilonGreedyPolicy(Q, epsilon, num_actions):
-        def policyFunction(state):
-    
-            Action_probabilities = np.ones(num_actions,
-                    dtype = float) * epsilon / num_actions
-                    
-            best_action = np.argmax(Q[state])
-            Action_probabilities[best_action] += (1.0 - epsilon)
-            return Action_probabilities
+    def policyFunction(self, state):
+        action_probabilities = np.ones(len(self.strategies), dtype = float) * self.epsilon / len(self.strategies)       
+        best_action = np.argmax(self.Q[state['green'], state['yellow'], state['missing'], :])
+        action_probabilities[best_action] += (1.0 - self.epsilon)
+        return action_probabilities
    
-        return policyFunction
-        
-
-    def train(self,trial, iter):
-        pass
+    def train(self,trial = 100, iter = 100):
+        for i in range(iter):
+            board, letters = self.env.reset()
+            state = get_state(letters) 
+            done = False
+            while(not done):
+                action_probabilities = self.policyFunction(state)
+                action_strategy = np.random.choice(np.arange(len(action_probabilities)), p = action_probabilities)
+                action = self.strategies[action_strategy].get_action()
+                action = word_to_action(action)
+                next, reward, done, _ = self.env.step(action)
+                next_state = get_state(next['letters'])
+                next_best_action = np.argmax(self.Q[next_state['green'], next_state['yellow'], next_state['missing'], :])
+                q_target = reward + self.gamma * self.Q[next_state['green'], next_state['yellow'], next_state['missing'], next_best_action]
+                self.Q[state['green'], state['yellow'], state['missing'], action_strategy] = q_target -  self.Q[state['green'], state['yellow'], state['missing'], action_strategy]
+                state = next_state
+            print(self.Q)
 
     def test(self):
         pass
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,5 @@
 import json
-from colorama import Fore
+import numpy as np
 
 def get_words(filename):
     words = None
@@ -19,13 +19,13 @@ def word_to_action(word):
 def action_to_word(action):
     return [chr(ord('A') + c) for c in action]
 
-def get_state(guess, word, state=None):
-    state = {'green': 0, 'yellow': 0, 'grey': 0} if state is None else state
-    for i, c in enumerate(guess):
-        if c == word[i]:
-            state['green'] += 1
-        elif c in word:
+def get_state(letters):
+    state = {'green': 0, 'yellow': 0, 'missing': 0} 
+    for letter in letters:
+        if letter == 0:
+            state['missing'] += 1
+        elif letter == 1:
             state['yellow'] += 1
-        else:
-            state['grey'] += 1
+        elif letter == 2:
+            state['green'] += 1
     return state
