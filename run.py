43d986930528b8fa7a0fa63f9c9d18c88cedf28e
astronights <shubhankar.a31@gmail.com>
2022-10-30 22:32:01 +0800

    Run change

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from src.models import no_learn_model, qlearn
 
 def main():
     # model = no_learn_model.NoLearnModel()
-    model = qlearn.QLearn({'epsilon': 0.8, 'gamma': 0.6})
+    model = qlearn.QLearn({'epsilon': 0.8, 'gamma': 0.6, 'alpha': 0.5})
     model.train(10)
     model.test()
 
